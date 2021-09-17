492acfca8ff73fd64a37aaabd7640d2a2603f62d
Ramesh Verma <rsverma333@gmail.com>
2021-09-17 11:23:29 +0530

    Added basic crop effect

---
+++ OD.DesktopUI/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OD.DesktopUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Canvas_DrawAnimated(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            float radius = (float)(1 + Math.Sin(args.Timing.TotalTime.TotalSeconds)) * 500f;
+            crop.SourceRectangle = new Rect(0, 0, radius, radius);
+            //blur.BlurAmount = radius;
+            args.DrawingSession.DrawImage(crop);
+        }
+        CropEffect crop;
+        GaussianBlurEffect blur;
+        private void canvas_CreateResources(
+            CanvasAnimatedControl sender,
+            Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            CanvasCommandList cl = new CanvasCommandList(sender);
+            using (CanvasDrawingSession clds = cl.CreateDrawingSession())
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    clds.DrawText("Hello, World!", RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawCircle(RndPosition(), RndRadius(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawLine(RndPosition(), RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                }
+            }
+            crop = new CropEffect()
+            {
+                Source = cl
+            };
+
+            blur = new GaussianBlurEffect()
+            {
+                Source = cl
+            };
+        }
+        Random rnd = new Random();
+        private Vector2 RndPosition()
+        {
+            double x = rnd.NextDouble() * 500f;
+            double y = rnd.NextDouble() * 500f;
+            return new Vector2((float)x, (float)y);
+        }
+
+        private float RndRadius()
+        {
+            return (float)rnd.NextDouble() * 150f;
+        }
+
+        private byte RndByte()
+        {
+            return (byte)rnd.Next(256);
+        }
+    }
+}
