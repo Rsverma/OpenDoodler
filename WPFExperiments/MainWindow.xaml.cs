94fedda17d94daad3f9fca782ae58e512ef85b3d
Ramesh Verma <rsverma333@gmail.com>
2021-09-21 14:23:00 +0530

    Added SVG path based hand and drawing animation

---
+++ WPFExperiments/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFExperiments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFExperiments/Page1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFExperiments
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent(); 
+            CaptureMyScreen();
+        }
+
+        public static readonly DependencyProperty PointProperty = DependencyProperty.Register(
+            "Point", typeof(Point), typeof(Page1), new FrameworkPropertyMetadata(new Point(0,0), OnPropertiesChanged));
+        private static void OnPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Page1 path)
+            {
+                path.UpdatePath((Point)e.NewValue);
+            }
+        }
+
+        private void UpdatePath(Point newValue)
+        {
+            this.arc1.Points.Add(newValue);
+        }
+
+        public Point Point
+        {
+            get => (Point)GetValue(PointProperty);
+            set => SetValue(PointProperty, value);
+        }
+        private void CaptureMyScreen()
+        {
+            string data = "M50,150 c-25,-125 125,-125 125,-25 c0,-100 150,-100 125,25 c-25,75 -100,100 -125,175 c-25,-75 -100,-100 -125,-175";
+            Geometry geo1 = Geometry.Parse(data);
+            PathGeometry path = geo1.GetFlattenedPathGeometry();
+            MatrixTransform buttonMatrixTransform = new MatrixTransform();
+            aButton.RenderTransform = buttonMatrixTransform;
+
+            // Register the transform's name with the page
+            // so that it can be targeted by a Storyboard.
+            this.RegisterName("ButtonMatrixTransform", buttonMatrixTransform);
+
+            // Create a MatrixAnimationUsingPath to move the
+            // button along the path by animating
+            // its MatrixTransform.M50,150 c-25,-125 125,-125 125,-25 c0,-100 150,-100 125,25 c-25,75 -100,100 -125,175 c-25,-75 -100,-100 -125,-175
+            MatrixAnimationUsingPath matrixAnimation = new MatrixAnimationUsingPath();
+            matrixAnimation.PathGeometry = path;
+
+            // Set IsOffsetCumulative to true so that the animation
+            // values accumulate when its repeats.
+            matrixAnimation.IsOffsetCumulative = false;
+            matrixAnimation.Duration = TimeSpan.FromSeconds(3);
+            matrixAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            // Set the animation to target the Matrix property
+            // of the MatrixTransform named "ButtonMatrixTransform".
+            Storyboard.SetTargetName(matrixAnimation, "ButtonMatrixTransform");
+            Storyboard.SetTargetProperty(matrixAnimation, new PropertyPath(MatrixTransform.MatrixProperty));
+
+            // Create a Storyboard to contain and apply the animation.
+            Storyboard pathAnimationStoryboard = new Storyboard();
+            pathAnimationStoryboard.Children.Add(matrixAnimation);
+            pathAnimationStoryboard.Begin(this);
+        }
+    }
+}
