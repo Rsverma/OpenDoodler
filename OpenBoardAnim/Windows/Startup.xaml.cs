a03ea2beb1e3220a74c66a35ba6f6ca9d8e769b8
Ramesh Verma <rsverma333@gmail.com>
2022-01-30 23:16:46 +0530

    Updated main screen UI

--- OpenBoardAnim/Windows/Startup.xaml.cs
+++ OpenBoardAnim/Windows/Startup.xaml.cs
@@ -15,10 +15,33 @@ namespace OpenBoardAnim.Windows.Other
 
         #region Events
 
+        private void Startup_OnLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+        private void Buttons_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
         private void Editor_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             var editor = new Editor();
             GenericShowDialog(editor);
+        }
+
+        private void Options_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //var options = new Options { Owner = this };
+            //options.ShowDialog();
+        }
+
+        private void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var test = new TestField();
+            test.ShowDialog();
         }
 
         #endregion
